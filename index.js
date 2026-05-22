0ea00d55eb60b04cfcc326fc83c61f71f9b2b91a
juhairulislam <juhairulislam2018@gmail.com>
2026-05-22 21:44:02 +0600

    add mongodb dirvers raw code

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ const app = express();
 app.use(cors())
 const port = process.env.PORT || 8080;
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.f4ctco1.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -13,3 +42,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// ideavault
+// QXcvTMD8cyelD3K6
