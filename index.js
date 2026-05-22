62eee66e880cbd4a0b3b12db05a58ee63d6a0f3e
juhairulislam <juhairulislam2018@gmail.com>
2026-05-22 22:08:24 +0600

    access db and collections from mongodb

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+
+
+    const db = client.db("ideavault") ;
+    const ideasCollections = db.collection("ideas")
+
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
