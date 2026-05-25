f4c46211c4756182ccec72c9feb10f3f098d9b6d
juhairulislam <juhairulislam2018@gmail.com>
2026-05-26 05:36:27 +0600

    set verify token in dynamic details route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 dotenv.config()
 const app = express()
 app.use(cors())
-const port =process.env.PORT || 8080
+const port = process.env.PORT || 8080
 
 
 
@@ -21,6 +21,18 @@ const client = new MongoClient(uri, {
   }
 });
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} || ${req.url}`)
+  next();
+
+}
+
+const verifyToken =async (req, res , next)=>{
+
+  console.log(req.headers , "from verify token")
+  next() ;
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,41 +40,35 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    const db = client.db('ideavault') ;
-    const ideasCollection = db.collection('ideas') ;
+    const db = client.db('ideavault');
+    const ideasCollection = db.collection('ideas');
 
-    app.get('/ideas' , async(req , res) =>{
+    app.get('/ideas', async (req, res) => {
 
-        const cursor = ideasCollection.find() ;
-        const result = await cursor.toArray() ;
-        res.send(result)
+      const cursor = ideasCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
     // dynamic ideas from Id
-    app.get('/ideas/:ideasId' , 
-      (req , res , next)=>{
-        console.log(req.params)
-        next() ;
+    app.get('/ideas/:ideasId', logger, verifyToken , async (req, res) => {
 
-      }
-       , async(req , res) =>{
-
-        const {ideasId} = req.params ;
-        const query = {_id: new ObjectId(ideasId)} 
-        const result = await ideasCollection.findOne(query) ;
+        const { ideasId } = req.params;
+        const query = { _id: new ObjectId(ideasId) }
+        const result = await ideasCollection.findOne(query);
         res.send(result)
 
 
-    })
+      })
 
 
     // trending section API
 
-       app.get('/trending' , async(req , res) =>{
+    app.get('/trending', async (req, res) => {
 
-        const cursor = ideasCollection.find().limit(6) ;
-        const result = await cursor.toArray() ;
-        res.send(result)
+      const cursor = ideasCollection.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
 
